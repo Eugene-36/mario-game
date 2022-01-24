194e2b692b8c227f525458ba084d561b25b25701
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2022-01-24 19:56:49 +0200

    added images

--- index.js
+++ index.js
@@ -70,6 +70,8 @@ const keys = {
   },
 };
 
+let scrollOffset = 0;
+
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
@@ -86,11 +88,13 @@ function animate() {
     player.velocity.x = 0;
 
     if (keys.right.pressed) {
+      scrollOffset += 5;
       platforms.forEach((platform) => {
         //platform.draw();
         platform.position.x -= 5;
       });
     } else if (keys.left.pressed) {
+      scrollOffset -= 5;
       platforms.forEach((platform) => {
         platform.position.x += 5;
       });
@@ -109,6 +113,10 @@ function animate() {
       player.velocity.y = 0;
     }
   });
+
+  if (scrollOffset > 2000) {
+    console.log('You win');
+  }
 }
 
 animate();
